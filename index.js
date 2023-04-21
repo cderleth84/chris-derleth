804d1740067ddd19c96d2d484210501f70fb25a8
Chris <cderleth84@me.com>
2023-04-21 16:22:25 -0700

    asdf

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
-// Get the navbar element
-var navbar = document.getElementById("navbar");
+const fs = FileSystem();
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-window.onscroll = function () {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-};
-
-// window.addEventListener("scroll", function () {
-//   const parallax = document.querySelector(".parallax");
-//   let scrollPosition = window.pageYOffset;
-//   parallax.querySelector(
-//     ".parallax__background"
-//   ).style.transform = `translateY(${scrollPosition * 0.5}px)`;
-// });
-
-function MyComponent() {
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-      <input type="text" placeholder="Enter your name" />
-    </div>
-  );
-}
+console.log(fs);

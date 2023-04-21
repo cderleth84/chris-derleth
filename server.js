ea211d119255024a6534d5e276dedc8ff6cabfbf
Chris <cderleth84@me.com>
2023-04-21 11:41:41 -0700

    trying to figure out serving up these files

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express();
 const path = require("path");
 
 // Static Files
-// app.use(express.static("dervelopment"));
-// app.use(express.static("public"));
-// app.use(express.static("public/css"));
-// app.use(express.static("public/img"));
+app.use(express.static("dervelopment"));
+app.use(express.static("public"));
+app.use(express.static("public/css"));
+app.use(express.static("public/img"));
 
 // Use this when wanting to get different website pages
 // app.use("/", express.static(path.join(__dirname + "dervelopment")));
@@ -15,23 +15,28 @@ const path = require("path");
 // Use this when serving files from a different directory
 // app.use(express.static(path.join(__dirname, "dervelopment")));
 
+// app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public/css")));
+// app.use(express.static(path.join(__dirname, "public/js")));
+// app.use(express.static(path.join(__dirname, "public/img")));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public/views/index.html"));
+// });
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+  console.log(res.sendFile(__dirname + "/views/index.html"));
+});
+
 // 404 Error Message
 app.use((req, res) => {
   res.status(404);
-  res.send("<h1>Error 404: Resource not found</h1>");
+  res.send("<h1>Error 404: Resource not found!</h1>");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
-
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "public/css")));
-app.use(express.static(path.join(__dirname, "public/js")));
-app.use(express.static(path.join(__dirname, "public/img")));
-
-app.get("/", (req, res) => {
-  res.send(path.join(__dirname, "/views/index.html"));
 });

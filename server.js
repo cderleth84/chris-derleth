8c57394161a8d3b482252fccb50650f4b3df1756
Chris <cderleth84@me.com>
2023-04-20 22:59:13 -0700

    moved around the server js

--- server.js
+++ server.js
@@ -9,15 +9,6 @@ const path = require("node:path");
 // app.use(express.static("public/css"));
 // app.use(express.static("public/img"));
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "public/css")));
-app.use(express.static(path.join(__dirname, "public/js")));
-app.use(express.static(path.join(__dirname, "public/img")));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/index.html"));
-});
-
 // Use this when wanting to get different website pages
 // app.use("/", express.static(path.join(__dirname + "dervelopment")));
 
@@ -34,4 +25,13 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public/css")));
+app.use(express.static(path.join(__dirname, "public/js")));
+app.use(express.static(path.join(__dirname, "public/img")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/index.html"));
 });

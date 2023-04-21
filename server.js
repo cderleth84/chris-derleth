84693d46379de0394c5095e44ed75493a1483d1a
Chris <cderleth84@me.com>
2023-04-21 14:56:18 -0700

    asdf

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static("public/img"));
 // Use this when serving files from a different directory
 // app.use(express.static(path.join(__dirname, "dervelopment")));
 
-app.use("/", express.static(path.join(__dirname, "views")));
+app.use("/home", express.static(path.join(__dirname, "views")));
 app.use(express.static(path.join(__dirname, "public/css")));
 app.use(express.static(path.join(__dirname, "public/img")));
 // app.use(express.static(path.join(__dirname, "public/js")));
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, "public/img")));
 //   res.send("Welcome to root URL of Server");
 // });
 
-app.get("/", (req, res) => {
+app.get("/home", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 

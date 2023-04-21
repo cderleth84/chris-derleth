19514f1b261b7edb7efae847ba4d3c4455de6cd0
Chris <cderleth84@me.com>
2023-04-21 14:54:57 -0700

    asdf

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static("public/img"));
 // Use this when serving files from a different directory
 // app.use(express.static(path.join(__dirname, "dervelopment")));
 
-// app.use("/home", express.static(path.join(__dirname, "views")));
+app.use("/", express.static(path.join(__dirname, "views")));
 app.use(express.static(path.join(__dirname, "public/css")));
 app.use(express.static(path.join(__dirname, "public/img")));
 // app.use(express.static(path.join(__dirname, "public/js")));

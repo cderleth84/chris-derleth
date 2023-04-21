8a0d3a32975cb4f4f429d54d949135165b821daf
Chris <cderleth84@me.com>
2023-04-20 20:37:04 -0700

    Hopefully fixed the server so now it will serve static files from the 'dervelopment' directory

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
-app.use(express.static("dervelopment"));
+app.use(express.static("../dervelopment"));
+// app.use(express.static(path.join(__dirname, "dervelopment")));
 
-app.get("./dervelopment", (req, res) => {
-  res.send("Hello World!");
+// 404 Error Message
+app.use((req, res) => {
+  res.status(404);
+  res.send("<h1>Error 404: Resource not found</h1>");
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });

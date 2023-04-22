31a8aa6d437b2c52ff1c6f4791be7c3817ae6cb0
Chris <cderleth84@me.com>
2023-04-21 20:17:33 -0700

    hgfd

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static("dervelopment"));
 // Use this when serving files from a different directory
 app.use(express.static(path.join(__dirname, "/public")));
-// app.use(express.static(path.join(__dirname, "views")));
+app.use(express.static(path.join(__dirname, "views")));
 
 // app.get("/", (req, res) => {
 //   res.status(200);

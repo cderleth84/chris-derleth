3acf4eadb25ec18eaedeb86e315a2ffe5aa97ede
Chris <cderleth84@me.com>
2023-04-21 18:50:34 -0700

    Tried connecting the RPS page as well as linking the JS file for the RPS logic. Let's see if it works

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const path = require("path");
 // app.use(express.static("public/css"));
 // app.use(express.static("public/img"));
 
+// JSON Files
+app.use(express.json());
+
 // Use this when wanting to get different website pages
 // app.use("/", express.static(path.join(__dirname + "dervelopment")));
 
@@ -32,12 +35,15 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/rps", (req, res) => {
+  res.sendFile(__dirname + "/views/rps.html");
+});
+
 app.get("/hello", (req, res) => {
   res.set("Content-Type", "text/html");
   res.status(200).send("<h1>Hello GFG Learner!</h1>");
 });
 
-app.use(express.json());
 app.post("/", (req, res) => {
   const { name } = req.body;
   res.send(`Welcome ${name}`);
